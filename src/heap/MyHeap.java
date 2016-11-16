b44bf50a6651f699ac1ae82b18ed91501e9be711
Will Graham <grahamw0@students.rowan.edu>
2016-11-16 14:04:50 -0500

    Commented

--- src/heap/MyHeap.java
+++ src/heap/MyHeap.java
@@ -1,13 +1,15 @@
 package heap;
 
-
+/**
+ * Supplied interface for a min-heap.
+ */
 public interface MyHeap {
-    public Node makeHeap(Comparable value);
-    public boolean isEmpty();
-    public boolean insert(Comparable value);
-    public boolean deleteMin();
-    public boolean decreaseKey(Node key, Comparable updateValue);
-    public boolean delete(Node del);
-    public boolean union(MyHeap heap);
-    public Comparable findMin();
+  public Node makeHeap(Comparable value);
+  public boolean isEmpty();
+  public boolean insert(Comparable value);
+  public boolean deleteMin();
+  public boolean decreaseKey(Node key, Comparable updateValue);
+  public boolean delete(Node del);
+  public boolean union(MyHeap heap);
+  public Comparable findMin();
 }
